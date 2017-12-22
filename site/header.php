8871230c69add68c71b6d51b25b24dd86ef7227a
alek6d <34475471+alek6d@users.noreply.github.com>
2017-12-22 10:17:41 +0000

    Update header.php

--- site/header.php
+++ site/header.php
@@ -8,14 +8,22 @@
 
 		<body>
 		<header>
-		<img class="headerimg" src="logorond.png">
-		<p><ul class="headerul">
-        <li class= "headerli"><a href="accueil.php" class = "headera">Accueil</a></li>
-        <li class= "headerli"><a href="capteurs.php" class = "headera">Capteurs & Actionneurs</a></li>
-				<li class= "headerli"><a href="catalogue.php" class = "headera">Catalogue</a></li>
-				<li class= "headerli"><a href="deconnexion.php" class = "headera">Déconnexion</a></li>
-        </ul>
 
-    </p>
-    </header>
-		</body>
+
+					<img class="headerimg" src="logorond.png">
+
+
+		<p><ul class="headerul">
+			<div class="leftone">
+					<li class= "headerli"><a href="accueil.php" class = "headera">Accueil</a></li>
+	       	 			<li class= "headerli"><a href="capteurs.php" class = "headera">Capteurs & Actionneurs</a></li>
+					<li class= "headerli"><a href="catalogue.php" class = "headera">Catalogue</a></li>
+			</div>
+	        	<div class="rightone">
+	        			<li class= "headerli1"><a href="deconnexion.php" class = "headera">Déconnexion</a></li>
+	       		 </div>
+
+		</ul>
+
+	    	</p>
+
