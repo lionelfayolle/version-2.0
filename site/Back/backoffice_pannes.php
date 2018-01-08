50d684784ddbe65de94fb731aa8ce8332bffb2b1
waterloowassaninsidejob <33628702+waterloowassaninsidejob@users.noreply.github.com>
2018-01-08 14:40:46 +0100

    Update backoffice_pannes.php

--- site/Back/backoffice_pannes.php
+++ site/Back/backoffice_pannes.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width">
 	<title>Pannes</title>
-	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="backoffice_pannes.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <?php include("header_bo.php"); ?>
@@ -24,7 +28,7 @@
 		die('Erreur : '.$e->getMessage());
 	}
 
-	$reponse = $bdd->query('SELECT idPannes, Date, Type_de_panne, Devices_idDevices FROM pannes ORDER BY Date'); // sélection des données dans la BDD pour inclusion dans le tableau
+	$reponse = $bdd->query('SELECT idPannes, Date, Type_de_panne, Devices_idDevices FROM pannes ORDER BY idPannes'); // sélection des données dans la BDD pour inclusion dans le tableau
 
 	echo "<table border=1>
 	<tr>
@@ -48,6 +52,53 @@
 	}
 	echo "</table>"; // fermeture du tableau
 ?>
+<p>
+
+
+<article>
+<div class="container">
+<!-- Trigger the modal with a button -->
+			<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Nouvelle panne</button> <!-- bouton d'ouverture de fenetre modale, ajout de domicile -->
+
+<!-- Modal -->
+			<div class="modal fade" id="myModal2" role="dialog">
+				<div class="modal-dialog modal-lg">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Panne</h4>
+						</div>
+						<form  method="post" action="post_panne.php">
+							<div class="modal-body">
+									<p>
+									Entrez les informations sur la nouvelle panne
+									<br>
+									<input type="number" placeholder="idPannes" name ="idPannes">
+									<br>
+									<input type="date" placeholder="Date" name="Date">
+									<br>
+									<select name="Type_de_panne" >
+           							<option value="surcharge">Surcharge</option>
+           							<option value="surchauffe">Surchauffe</option>
+           							<option value="degats_des_eaux">Dégats des eaux</option>
+           							</select>
+									<br>
+									<input type="number" placeholder="Coût occasionné" name="Cout_ocasionne">
+									<br>
+									<input type="number" placeholder="idDevices" name="Devices_idDevices">
+									<br>
+									<input type="submit" value = "Ajouter">
+							</div>
+						<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+						</div>
+					</form>
+			</div>
+	</div>
+</div>
+</div>
+
+	</article>
 </center>
 		</article>
 
