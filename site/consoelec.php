af4ba00c1482d659723a734991691701291ccc1d
MatthieuWlt <willot.matthieu@gmail.com>
2018-01-08 16:58:47 +0100

    Update consoelec.php

--- site/consoelec.php
+++ site/consoelec.php
@@ -1,22 +1,76 @@
 <?php
 session_start();
-
 ?>
+
 <!DOCTYPE html>
 	<html>
-		<head>
+	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width">
 		<link rel="stylesheet" href="Categorie_capteurs.css"/>
-		<title>Capteurs de consommation électrique</title>
-		</head>
-		<?php include("header.php"); ?>
-		<body>
-			<form method="post" action="traitement.php">
+		<title>Capteurs de consommation d'eau</title>
+	</head>
+
+<?php include("header.php"); ?>
+		
+	<body>
+
+
+		<article>
+		<center><h1>Liste des capteurs en panne</h1></center>
+
+		<center>
+
+			<?php
+				// Connexion à la base de données
+				try
+				{
+					$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+				}
+				catch(Exception $e)
+				{
+					die('Erreur : '.$e->getMessage());
+				}
+
+				//requête pour récupérer les info triées par date depuis la table pannes
+				$reponse = $bdd->query('SELECT * FROM type_de_device WHERE type_de_capteur = "Electricite"');
+
+				// Affichage de ces informations dans un tableau
+				echo "<table border=1>
+				<tr>
+					<td> <strong>Nom</strong> </td>
+					<td> <strong>Fonction</strong> </td>
+					<td> <strong>Type de capteur</strong> </td>
+					<td> <strong>Prix</strong> </td>
+				</tr>
+				";
+				
+				while ($donnees = $reponse->fetch())
+				{
+					echo "
+					<tr>
+						<td>".$donnees["nom_du_device"]."</td>
+						<td>".$donnees["fonction"]."</td>
+						<td>".$donnees["type_de_capteur"]."</td>
+						<td>".$donnees["prix"]."</td>
+				     </tr>
+				     	";
+				}
+				echo "</table>"; 
+			?>
+
+		</center>
+		</article>
+
+
+
+
+			<!-- Formulaire pour commander un article -->
+		<form method="post" action="traitement.php">
 			<div align="right">
-				<label>Veuillez entrer l'identifiant de votre commande</label> : <input type="text" name="commande" />
+				<label>Veuillez entrer l'identifiant de votre commande : </label><input type="text" name="commande" />
 				<input type="submit" value="Envoyer" />
-			</div> .<p>
-			</form>
+			</div>
+		</form>
 		<?php include("footer.php"); ?>
-		</body>
+	</body>
