1972ce6d1f17889a17a4f46a3f344df3061064e4
lionelfayolle <33628543+lionelfayolle@users.noreply.github.com>
2018-01-11 09:54:53 +0100

    Update accueil.php

--- site/accueil.php
+++ site/accueil.php
@@ -6,6 +6,40 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+  <script>
+            function showUser(str)
+            {
+                if (str == "")
+                {
+                    document.getElementById("txtHint").innerHTML = "";
+                    return;
+                }
+                if (window.XMLHttpRequest) {
+                    xmlhttp= new XMLHttpRequest();
+                } else {
+                    if (window.ActiveXObject)
+                        try {
+                            xmlhttp= new ActiveXObject("Msxml2.XMLHTTP");
+                        } catch (e) {
+                            try {
+                                xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+                            } catch (e) {
+                                return NULL;
+                            }
+                        }
+                }
+
+                xmlhttp.onreadystatechange = function ()
+                {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+                    {
+                        document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "getuser.php?q=" + str, true);
+                xmlhttp.send();
+            }
+        </script>
   <title>Connect'Home</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,43 +54,21 @@ session_start();
     <center><h1>Accueil</h1></center>
     <article>
         <h1>Informations</h1><hr>
-        <p>Identifiant : <br> Nom : <?php echo $_SESSION['nom']?><br> Prénom : <?php echo $_SESSION['prenom']?><br> Adresse : <br> Mail : <?php echo $_SESSION['mail']?></p>
+        <p>Identifiant : <?php echo $_SESSION['id']?><br> Nom : <?php echo $_SESSION['nom']?><br> Prénom : <?php echo $_SESSION['prenom']?><br> Adresse : <?php echo $_SESSION['num_rue'].' '. $_SESSION['nom_rue'].' '.$_SESSION['code_postal'].' '.$_SESSION['ville']?> <br> Mail : <?php echo $_SESSION['mail']?></p>
     </article>
 
     <article>
         <h1>Pièces</h1><hr>
-        <p>Liste des pièces et leur superficie :<br></p>
-        <?php
-        // Connexion à la base de données
-        try
-        {
-          $bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
-        }
-        catch(Exception $e)
-        {
-                die('Erreur : '.$e->getMessage());
-        }
-
-        // Récupération des 10 derniers messages
-        $reponse = $bdd->query('SELECT nom_piece, surface FROM piece ORDER BY nom_piece');
-
-        // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-        while ($donnees = $reponse->fetch())
-        {
-          echo '<p><strong>' . htmlspecialchars($donnees['nom_piece']) . '</strong> : ' . htmlspecialchars($donnees['surface']) . '</p>';
-        }
-
-        $reponse->closeCursor();
-
-        ?>
+        
+          
         <div class="container">
 
-        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" style="position:relative;bottom:150px;margin-left:5px">Ajouter une pièce</button>
-        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal3" style="position:relative;bottom:150px;">Supprimer une pièce</button>
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" style="position:relative;bottom:80px;margin-left:5px">Ajouter une pièce</button>
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal3" style="position:relative;bottom:80px;">Supprimer une pièce</button>
 
         <div class="modal fade" id="myModal" role="dialog">
             <div class="modal-dialog modal-lg">
-                <div class = "piececontent">
+                <div class="modal-content">
                 <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
           <h4 class="modal-title">Ajouter une pièce</h4>
@@ -86,10 +98,42 @@ session_start();
       <p>Entrez les informations sur votre pièce à supprimer<br><input type="text" name="nompiece" id="nompiece" placeholder="Nom de la pièce"><br>
       <input type="submit" name="supprimer" value="Supprimer"></p>
       </form>
-  </div>
-
+       </div>
+      </div>
+    </div>
 </div>
 
+        <div id="txtHint"><b>Liste des pièces :</b></div>
+        <?php
+        // Connexion à la base de données
+        try
+        {
+          $bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+
+        // Récupération des 10 derniers messages
+        $reponse = $bdd->prepare('SELECT Nom_piece, Surface_piece FROM piece WHERE Appartement_Code_appartement = ?');
+        $reponse->execute(array($_SESSION['code_appart']));
+
+        // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+        ?>
+        <form class="select">
+          <select name='users' onchange='showUser(this.value)'>
+          <option value=''>Selectionne une pièce</option>
+        <?php 
+        while ($donnees = $reponse->fetch())
+        {
+          echo "<option value='" .$donnees['Nom_piece']. "'>".$donnees['Nom_piece']."</option>";
+        }
+
+        $reponse->closeCursor();
+
+        ?>
+        </select></form>
     </article>
 
     <article>
@@ -99,6 +143,7 @@ session_start();
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2" style="position:relative;bottom:120px; margin-left:5px">Ajouter capteurs & actionneurs</button>
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4" style="position:relative;bottom:120px;">Supprimer un capteur</button>
+
   <?php
   // Connexion à la base de données
   try
@@ -111,10 +156,12 @@ session_start();
   }
 
   // Récupération des 10 derniers messages
-  $reponse = $bdd->query('SELECT reference, fonction FROM capteur ORDER BY fonction');
+  $reponse = $bdd->prepare('SELECT * FROM devices NATURAL JOIN type_de_device WHERE devices.Type_de_device_reference_produit=type_de_device.reference_produit AND CeMac_idCeMac=?');
+  $reponse->execute(array($_SESSION['idcemac']));
 
   echo "<table border=1>
   <tr>
+    <td> Nom </td>
     <td> Fonction </td>
     <td> Réference </td>
   </tr>
@@ -123,8 +170,9 @@ session_start();
   {
     echo "
     <tr>
+     <td>".$donnees["nom_du_device"]."</td>
      <td>".$donnees["fonction"]."</td>
-     <td>".$donnees["reference"]."</td>
+     <td>".$donnees["reference_produit"]."</td>
      </tr>
      ";
   }
@@ -144,7 +192,7 @@ session_start();
         </div>
         <div class="modal-body">
           <form class="black" method="post" action="accueil3.php">
-            <p>Entrez les informations sur votre nouveau capteur<br><input type="text" name="reference" placeholder="Référence du capteur">
+            <p>Entrez les informations sur votre nouveau capteur<br><input type="text" id="reference" name="reference" placeholder="Référence du capteur">
             <select name="type" id="type">
               <option value="Humidite">Humidité</option>
               <option value="Camera">Caméra</option>
@@ -154,6 +202,7 @@ session_start();
               <option value="Consommation d'eau">Consommation d'eau</option>
               <option value="Electricite">Electricité</option>
             </select>
+            <input type="text" id="piece" name="piece" placeholder="Pièce">
             </p>
             <input type="submit" name="ajouter" value="Ajouter">
         </form>
